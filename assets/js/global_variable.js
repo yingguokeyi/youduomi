350cad46fee5e16ab620217aa84962613064d591
刘学 <1725948799@qq.com>
2018-11-05 10:56:37 +0800

    项目

---
+++ assets/js/global_variable.js
@@ -0,0 +1,3 @@
+// var domain_name_url = 'http://10.1.0.19:8056/poseidon';//小沐
+var domain_name_url = 'http://10.1.0.18:8056/poseidon';//画生
+// var domain_name_url = 'http://10.1.0.113:8082/poseidon';//测试
