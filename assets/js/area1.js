56c75828208ff428b677031471b69e06d132a57d
刘学 <1725948799@qq.com>
2018-11-10 18:19:02 +0800

    个人中心页面更改

--- assets/js/area1.js
+++ assets/js/area1.js
@@ -418,10 +418,7 @@ dsy.add("0_33",["台北","高雄","台中","花莲","基隆","嘉义","金门","
 dsy.add("0",["北京市","天津市","上海市","重庆市","河北省","山西省","内蒙古","辽宁省","吉林省","黑龙江省","江苏省","浙江省","安徽省","福建省","江西省","山东省","河南省","湖北省","湖南省","广东省","广西","海南省","四川省","贵州省","云南省","西藏","陕西省","甘肃省","青海省","宁夏","新疆","香港","澳门","台湾省"]);
 
 var s=["s_province","s_city","s_county"];//三个select的name
-var myProx = localStorage.getItem('myProx');
-var myCityx = localStorage.getItem('myCityx');
-var myAreax = localStorage.getItem('myAreax');
-var opt0 = [myProx,myCityx,myAreax];//初始值
+var opt0 = ["","",""];//初始值
 function _init_area(){  //初始化函数
 	for(i=0;i<s.length-1;i++){
 	  document.getElementById(s[i]).onchange=new Function("change("+(i+1)+")");
