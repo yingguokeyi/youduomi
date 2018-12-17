864e14a354abc0b3f785025a89c7a93c288dabcc
刘学 <1725948799@qq.com>
2018-12-17 19:25:53 +0800

    页面

--- js/mine/task_query.js
+++ js/mine/task_query.js
@@ -4,12 +4,14 @@ $(function(){
 	})
 	$('#year_sel ul li').click(function(){
 		$('.year').html($(this).html());
+		$('#year_sel').hide();
 	})
 	$('#cli_sel').click(function(){
 		$('#mont_sel').toggle();
 	})
 	$('#mont_sel ul li').click(function(){
 		$('.month').html($(this).html());
+		$('#mont_sel').hide();
 	})
 	
 })
